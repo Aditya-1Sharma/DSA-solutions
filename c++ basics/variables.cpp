0fc0fe528ef94b903714fdead323df4a06ca6a3f
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-03 23:19:55 +0530

    variables in cpp

---
+++ c++ basics/variables.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a=5; //integer datatype
+    cout <<"value of a is : " << a;
+    
+    string s; //string
+    char ch; //character
+    bool b = "True"; //boolean (true or false)
+    double d; // double 
+    float f; //float 
+    
+
+}
