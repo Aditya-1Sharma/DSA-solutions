8345f2576b90c450c80afe965f82319aa1854d9f
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-03 18:49:58 +0000

    operations in c++

---
+++ c++ basics/operations.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 10;
+
+    cout << a+b << endl ; //add operation
+    cout << a-b << endl ; //subtract operation
+    cout << a*b << endl ; //multiply operation
+    cout << a/b << endl ; //divide operation
+    cout << a%b << endl ; //modulus operation
+
+}
