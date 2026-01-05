2b8ab740d809440aa3e4bbad9dacb4db4b51e7b9
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-05 16:59:36 +0000

    functions in cpp

--- c++ basics/functions.cpp
+++ c++ basics/functions.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//function to get square of a number
+int func (int a){
+
+    //body
+    int result = a*a;
+    return result;
+
+}
+
 int main() {
 
-    
+    int n = 10;
+    int result = func(n);
+    cout << result << endl;
 
     return 0;
 
