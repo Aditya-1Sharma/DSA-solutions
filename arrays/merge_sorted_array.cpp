60b9e38ce6daf0fd312295d3a2e4525c70b69b86
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-11 17:04:13 +0000

    merge sorted array

--- arrays/merge_sorted_array.cpp
+++ arrays/merge_sorted_array.cpp
@@ -26,3 +26,51 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
             right--;
         }        
     }
+
+void printVector(const vector<int>& arr) {
+    cout << "[";
+    for (size_t i = 0; i < arr.size(); ++i) {
+        cout << arr[i];
+        if (i < arr.size() - 1) {
+            cout << ",";
+        }
+    }
+    cout << "]";
+}
+
+int main() {
+    // Example 1
+    vector<int> nums1_1 = {1,2,3,0,0,0};
+    int m1 = 3;
+    vector<int> nums2_1 = {2,5,6};
+    int n1 = 3;
+    cout << "Example 1:" << endl;
+    cout << "Input: nums1 = "; printVector(nums1_1); cout << ", m = " << m1;
+    cout << ", nums2 = "; printVector(nums2_1); cout << ", n = " << n1 << endl;
+    merge(nums1_1, m1, nums2_1, n1);
+    cout << "Output: "; printVector(nums1_1); cout << endl << endl;
+
+    // Example 2
+    vector<int> nums1_2 = {1};
+    int m2 = 1;
+    vector<int> nums2_2 = {};
+    int n2 = 0;
+    cout << "Example 2:" << endl;
+    cout << "Input: nums1 = "; printVector(nums1_2); cout << ", m = " << m2;
+    cout << ", nums2 = "; printVector(nums2_2); cout << ", n = " << n2 << endl;
+    merge(nums1_2, m2, nums2_2, n2);
+    cout << "Output: "; printVector(nums1_2); cout << endl << endl;
+
+    // Example 3
+    vector<int> nums1_3 = {0};
+    int m3 = 0;
+    vector<int> nums2_3 = {1};
+    int n3 = 1;
+    cout << "Example 3:" << endl;
+    cout << "Input: nums1 = "; printVector(nums1_3); cout << ", m = " << m3;
+    cout << ", nums2 = "; printVector(nums2_3); cout << ", n = " << n3 << endl;
+    merge(nums1_3, m3, nums2_3, n3);
+    cout << "Output: "; printVector(nums1_3); cout << endl << endl;
+
+    return 0;
+}
