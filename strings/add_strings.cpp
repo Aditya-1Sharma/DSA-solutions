1b54df53a12a08a4f478f40357853b3be3c9fcdb
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-13 16:27:42 +0000

    add_strings

---
+++ strings/add_strings.cpp
@@ -0,0 +1,33 @@
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger).
+// You must also not convert the inputs to integers directly.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+std::string addStrings(std::string num1, std::string num2) {
+        int i = num1.size() - 1, j = num2.size() - 1;
+        int carry = 0;
+        std::string result;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int digit1 = (i >= 0) ? num1[i] - '0' : 0;
+            int digit2 = (j >= 0) ? num2[j] - '0' : 0;
+
+            int total = digit1 + digit2 + carry;
+            carry = total / 10;
+
+            result.push_back((total % 10) + '0');
+
+            --i;
+            --j;
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+
+int main() {
+
+}
