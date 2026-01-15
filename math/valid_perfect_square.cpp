06328f5313be841a2001dd1cfb6614547c425429
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-15 17:40:13 +0000

    valid_perfect_square

---
+++ math/valid_perfect_square.cpp
@@ -0,0 +1,28 @@
+// Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+// A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+// You must not use any built-in library function, such as sqrt.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPerfectSquare(int num) {
+      for (long long i=1;i<=num;i++)  {
+        if(i*i==num ){
+            return true;
+        }
+        if(i*i>num) {
+            return false;
+        }
+        }
+        return 0;
+    }
+
+int main() {
+    int num;
+    cout << "Enter a positive integer : " << endl;
+    cin >> num;
+
+    int result = isPerfectSquare
+}
