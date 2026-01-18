438212f8294c38c1574aaa15b8ebadb8ec67ab01
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-18 16:44:52 +0000

    search a 2D matrix

--- arrays/search_a_2D_matrix.cpp
+++ arrays/search_a_2D_matrix.cpp
@@ -1,3 +1,14 @@
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+#include <bits/stdc++.h>
+using namespace std;
+
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(!matrix.size()) return false;
 
@@ -17,3 +28,28 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
         }
         return false;
     }
+
+int main() {
+    
+    int m,n;
+    cout << "Enter the number of rows : ";
+    cin >> m;
+    cout << "Enter the number of columns : ";
+    cin >> n;
+    
+    vector<vector<int>> matrix(m, vector<int>(n));
+    
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    
+    int target;
+    cout << "Enter the integer you want to search : ";
+    cin >> target;
+    
+    bool result = searchMatrix(matrix,target);
+    cout << result;
+    
+}
