6342f7120d78494d905c5aea3c25b31d8dd0800e
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-01-22 12:49:44 +0000

    merge_two_sorted_lists

---
+++ linkedlists/merge_two_sorted_lists.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ListNode* solve(ListNode* list1, ListNode* list2){
+        ListNode* curr1 = list1;
+        ListNode* next1 = curr1 -> next;
+        ListNode* curr2 = list2; 
+        ListNode* next2 = curr2 -> next;
+        
+        if(next1 == NULL){
+            curr1 -> next = curr2;
+            return list1;
+        }
+
+        while(next1 != NULL and curr2 != NULL){
+            if((curr2 -> val >= curr1-> val) and (curr2 -> val <= next1 -> val)){
+                curr1 -> next = curr2;
+                next2 = curr2 -> next;
+                curr2 -> next = next1;
+                curr1 = curr2;
+                curr2 = next2;
+            }
+            else{
+                curr1 = next1;
+                next1 = next1 -> next;
+                if(next1 == NULL){
+                    curr1 -> next = curr2;
+                    return list1;
+                }
+            }
+        }
+        return list1;
+    }
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+
+        if(list2 == NULL) return list1;
+
+        if(list1 -> val <= list2 -> val){
+            return solve(list1,list2);
+        }
+        else return solve(list2,list1);
+    }
+ 
+int main() {
+    
+}
