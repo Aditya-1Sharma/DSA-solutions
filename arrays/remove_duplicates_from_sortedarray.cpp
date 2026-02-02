02578a21e771f8eb1187dc27ae91bb297f35a750
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-02-02 17:07:36 +0000

    remove duplicates from sorted array

---
+++ arrays/remove_duplicates_from_sortedarray.cpp
@@ -0,0 +1,37 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+// The relative order of the elements should be kept the same.
+
+// Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        int swap = 1;
+
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[swap] = nums[i];
+                swap++;
+            }
+        }
+        return swap;
+    }
+
+int main() {
+    int n;
+    cout << "Enter the size of array : " << endl;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int result = removeDuplicates(nums);
+    cout << result ;
+
+    return 0;
+}
