6e799ca42f90b3562d534081564e8b87a826db42
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-02-18 18:14:08 +0000

    XOR operation in an array

---
+++ math/XOR_opp_inarray.cpp
@@ -0,0 +1,20 @@
+// You are given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums(n);
+        int x = 0;
+        for (int i = 0; i < n; i++)
+            nums[i] = start + 2 * i;
+        for (int i = 0; i < n; i++)
+            x ^= nums[i];
+
+        return x;
+    }
+};
