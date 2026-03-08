bb582b59e8cbe0d9e407bca9ee94703470bda769
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-03-08 18:24:33 +0000

    Find Unique Binary String

---
+++ strings/unique_binary_string.cpp
@@ -0,0 +1,27 @@
+// Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums.
+// If there are multiple answers, you may return any of them.
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        int size = pow(2, n);
+
+        vector<int> nu(size, 0);
+
+        for(string num : nums){
+            int val = stoi(num, nullptr, 2);
+            nu[val]++;
+        }
+
+        for(int i = 0; i < size; i++){
+            if(nu[i] == 0){
+                string ans = bitset<32>(i).to_string();
+                ans = ans.substr(32 - n);
+                return ans;
+            }
+        }
+
+        return string(n, '0');
+    }
+};
