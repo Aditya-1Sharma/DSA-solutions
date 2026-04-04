632f7aa071dacf861d4a5c73fc516a76c5fd95e2
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-04-04 17:35:13 +0000

    Letter combination of a phone number

---
+++ strings/letter_combination_ofa_phonenumber.cpp
@@ -0,0 +1,42 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
+        }
+        
+        unordered_map<char, string> m = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        string curr = "";
+        vector<string> result;
+        
+        dfs(digits, 0, m, curr, result);
+        return result;
+    }
+private:
+    void dfs(string digits, int index, unordered_map<char, string>& m, string& curr, vector<string>& result) {
+        if (index == digits.size()) {
+            result.push_back(curr);
+            return;
+        }
+        string str = m[digits[index]];
+        for (int i = 0; i < str.size(); i++) {
+            curr.push_back(str[i]);
+            dfs(digits, index + 1, m, curr, result);
+            curr.pop_back();
+        }
+    }
+};
