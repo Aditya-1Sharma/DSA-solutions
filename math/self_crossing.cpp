939ddbd161ff0ca3dfbecfd405ae894bb3932e6f
zonihansum <102897830+Aditya-1Sharma@users.noreply.github.com>
2026-04-06 16:23:58 +0000

    Self Crossing

---
+++ math/self_crossing.cpp
@@ -0,0 +1,23 @@
+// You are given an array of integers distance.
+
+// You start at the point (0, 0) on an X-Y plane, and you move distance[0] meters to the north, then distance[1] meters to the west, 
+// distance[2] meters to the south, distance[3] meters to the east, and so on. In other words, after each move, your direction changes counter-clockwise.
+
+// Return true if your path crosses itself or false if it does not.
+
+
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& distance) {
+        if(distance.size()<4) return false;
+        distance.insert(distance.begin(),0);
+        for(int i=3;i<distance.size();i++){
+            if(distance[i]>=distance[i-2] && distance[i-1]<=distance[i-3]) return true;
+            if(i>=5){
+                if(distance[i-1]<=distance[i-3] && distance[i-2]>=distance[i-4]&& distance[i-5]>=distance[i-3]-distance[i-1] && distance[i]>=distance[i-2]-distance[i-4])
+                return true;
+            }
+        }
+        return false;
+    }
+};
